26004878bfc7c37848b2086a33dfb9e27babee46
Benjamin Stalder <benjamin.stalder@swisscom.com>
2025-01-13 09:21:17 +0100

    new plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,7 +12,7 @@ vim.keymap.set("v", "L", "$", { noremap = true, silent = true })
 vim.keymap.set("n", "'", "`", { noremap = true, silent = true })
 vim.keymap.set("v", "'", "`", { noremap = true, silent = true })
 
-vim.keymap.set("n", "<leader>h", ":bprev<CR>", { silent = true, desc = "Jump to previous buffer" })
-vim.keymap.set("n", "<leader>l", ":bnext<CR>", { silent = true, desc = "Jump to next buffer" })
+-- vim.keymap.set("n", "<leader>h", ":bprev<CR>", { silent = true, desc = "Jump to previous buffer" })
+-- vim.keymap.set("n", "<leader>l", ":bnext<CR>", { silent = true, desc = "Jump to next buffer" })
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
